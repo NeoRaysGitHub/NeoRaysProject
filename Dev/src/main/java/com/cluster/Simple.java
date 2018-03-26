f59c19b293e0df79c42e502cabda15e719a91202
mkneorays <mithilesh.k@clusterindia.com>
2018-03-26 16:53:25 +0530

    add a sample code

---
+++ Dev/src/main/java/com/cluster/Simple.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.cluster;
+
+/**
+ * @author Neorays
+ *
+ */
+public class Simple {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	  System.out.println("cluster india");
+	}//main
+
+}//class
