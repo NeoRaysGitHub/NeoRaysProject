ba93905640214377e93b4f2b18733e8708e65ae3
NeoRays-PC\NeoRays <deepashree.m@clusterindia.com>
2018-03-26 15:38:56 +0530

    added new java file

---
+++ Dev/src/main/java/com/cluster/Demo_Deepa.java
@@ -0,0 +1,10 @@
+package com.cluster;
+
+public class Demo_Deepa {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		System.out.println("welcome to Neorays....");
+	}
+
+}
