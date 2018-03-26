bbcf90e1a30301b78721c88b7b5a57fcb77506dd
mkneorays <mithilesh.k@clusterindia.com>
2018-03-26 15:01:20 +0530

    first commit

---
+++ LoginApp/src/com/neorays/java/Simpe.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.neorays.java;
+
+/**
+ * @author Neorays
+ *
+ */
+public class Simpe {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+
+   System.out.println("mithilesh");
+	}//main
+
+}//class
